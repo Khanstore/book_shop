02b7e38aed9275a2c589208ac2c5159c613053e3
Khanstore <dmashraf@gmail.com>
2025-04-09 18:16:14 +0600

    sale order address corrected
    Invoicing address delivery address and partner address in sale order fixed

--- __manifest__.py
+++ __manifest__.py
@@ -31,6 +31,7 @@ governements, etc.)
         'views/invoice.xml',
         'reports/product_product_templates.xml',
         'reports/invoice.xml',
+        'reports/sale_order.xml',
         'data/book_shop_pages.xml',
         'wizard/wiard_views.xml',
         'reports/daily_admin_report.xml',
