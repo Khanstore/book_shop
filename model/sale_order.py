60613400fcacc8af104bf56ceb750d6231f22083
Khanstore <dmashraf@gmail.com>
2025-05-24 19:53:01 +0600

    whatsapp messege sending removed to another app

--- model/sale_order.py
+++ model/sale_order.py
@@ -46,7 +46,4 @@ class SaleOrder(models.Model):
         print ("yes")
         # Open the invoice record (example: returning its form view in Odoo)
 
-    def action_send_whatsapp(self):
-        to_number=self.partner_id.mobile
-        link="https://wa.me/"+to_number +"/?text="+"https://khan-store.com/my/orders/634?access_token=f41f3d0e-4c4c-4d20-923b-9f3e239496af"
-
+    
