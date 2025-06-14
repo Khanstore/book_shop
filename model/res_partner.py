9ce978c865e25f47ea18fff10f389002964ce160
Khanstore <dmashraf@gmail.com>
2025-06-14 20:35:57 +0600

    autopost bill field value in res_partner corrected corrected

--- model/res_partner.py
+++ model/res_partner.py
@@ -38,14 +38,14 @@ class Partner(models.Model):
     publisher_of = fields.Many2many(comodel_name="product.template",relation= 'publisher_book_rel',column1= 'publisher_ids', column2='publisher_of',
                                     string="Publisher Of")
 
-    autopost_bills = fields.Selection(
-        selection=[('always', 'Always'), ('ask', 'Ask after 3 validations without edits'), ('never', 'Never')],
-        string='Auto-post bills',
-        help="Automatically post bills for this trusted partner",
-        default='ask',
-        copy=False,
-        required=True,
-    )
+    # autopost_bills = fields.Selection(
+    #     selection=[('always', 'Always'), ('ask', 'Ask after 3 validations without edits'), ('never', 'Never')],
+    #     string='Auto-post bills',
+    #     help="Automatically post bills for this trusted partner",
+    #     default='ask',
+    #     copy=False,
+    #     required=True,
+    # )
     total_balance = fields.Monetary(
         string="Total Balance",
         compute="_compute_total_balance",
